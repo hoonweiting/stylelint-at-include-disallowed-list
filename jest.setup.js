c8112c5e03d839514dd475514d5432bc7b700737
Wei Ting <hoonweiting@gmail.com>
2023-11-13 22:55:16 +0800

    Update `files` in package.json

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// eslint-disable-next-line n/no-unpublished-require
 const { getTestRule } = require("jest-preset-stylelint");
 
 global.testRule = getTestRule({ plugins: ["./index.js"] });
